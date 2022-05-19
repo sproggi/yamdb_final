2d63f90af9d5b5bdce4eb2996037a6ff4cf61917
Alexey <sprygin.aleks@gmail.com>
2022-05-19 23:00:59 +0300

    redoc

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -5,7 +5,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(
-        'redoc/',
+        'static/redoc/',
         TemplateView.as_view(template_name='redoc.html'),
         name='redoc'
     ),
